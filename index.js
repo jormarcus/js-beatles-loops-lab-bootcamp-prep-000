459c8089a99518e0d18a7d0dad64288873362307
Jordan Marcus <jormarcus92@gmail.com>
2018-02-02 22:40:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(arrayM, arrayI) {
+  var arr = [];
+  for (var i = 0; i < arrayM.length; i++) {
+    arr.push(arrayM[i] + ' plays ' + arrayI[i]);
+  }
+  return arr;
+}
+
+function johnLennonFacts(factsArr) {
+  var newArr = [];
+  while (var i <= factsArr.length) {
+    newArr.push(factsArr[i] + '!!!')
+  }
+  return newArr;
+}
